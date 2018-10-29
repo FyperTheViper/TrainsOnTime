62fb25ba369fad38e3a0b6365f55f16d7281c3e8
FyperTheViper <b.w.byars@gmail.com>
2018-10-29 15:22:03 -0700

    groundwork added

---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyD_YOyZU-QxptbHgIMs7-_ncQNAdujhwgs",
+    authDomain: "ride-the-train-90321.firebaseapp.com",
+    databaseURL: "https://ride-the-train-90321.firebaseio.com",
+    projectId: "ride-the-train-90321",
+    storageBucket: "ride-the-train-90321.appspot.com",
+    messagingSenderId: "741549140791"
+  };
+  
+  firebase.initializeApp(config);
