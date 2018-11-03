2cd5cde01b2d4215f2c5dc1630e5867240a44300
FyperTheViper <b.w.byars@gmail.com>
2018-11-02 17:19:52 -0700

    JS most owrking need to fix moment.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
-let l = console.log()
+let l = console.log
+// Initialize Firebase
 var config = {
     apiKey: "AIzaSyD_YOyZU-QxptbHgIMs7-_ncQNAdujhwgs",
     authDomain: "ride-the-train-90321.firebaseapp.com",
@@ -12,7 +13,7 @@ var config = {
 
   firebase.initializeApp(config);
 
-let database = firebase.database();
+let trainDatabase = firebase.database();
 
 $("#gimme-train").on("click", function() {
 
@@ -21,6 +22,57 @@ $("#gimme-train").on("click", function() {
   let trainTime = $("#time-here").val().trim();
   let trainFrequency = $("#frequency-here").val().trim();
 
+  let trainAdded = {
+    name: trainName,
+    destination: trainPlace,
+    time: trainTime,
+    frequency: trainFrequency,
+  }
+
+  trainDatabase.ref().push(trainAdded);
+
+  $("#name-here").val("");
+  $("#place-here").val("");
+  $("#time-here").val("");
+  $("#frequency-here").val("");
+
+  //Added to prevent the page from reloading on submit
+  return false;
+
 })
+
+trainDatabase.ref().on("child_added", function(childSnapshot){
+
+let trainName = childSnapshot.val().name;
+let trainPlace = childSnapshot.val().destination;
+let trainTime = childSnapshot.val().time;
+let trainFrequency = childSnapshot.val().frequency;
+
+let timeCalc = moment(childSnapshot.val().trainTime, "hh:mm").subtract(1, "years");
+l(timeCalc);
+let minuteTime = moment().diff(moment(timeCalc), "minutes");
+l(minuteTime);
+let remainder = minuteTime % childSnapshot.val().trainFrequency;
+l(remainder);
+let minAway = childSnapshot.val().trainFrequency - remainder;
+l(minAway);
+let nextTrain = moment().add(minAway, "minutes");
+l(nextTrain);
+nextTrain = moment(nextTrain).format("hh:mm");
+
+
+
+$("#time-table").append("<tr><td>" + trainName + 
+  "</td><td>" + trainPlace + 
+  "</td><td>" + trainFrequency + 
+  "</td><td>" + nextTrain +
+  "</td><td>" + minAway + "</td></tr>");
+
+}, function(errorObject) {
+  l("Errors handled: " + errorObject.code);
+
+});
+
+
 
 })
