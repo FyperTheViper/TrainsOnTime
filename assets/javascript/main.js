1f45997894220e16d218548b0ae7e582b1bfd130
FyperTheViper <b.w.byars@gmail.com>
2018-11-03 22:34:49 -0700

    cleaned up CSS

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 
+//for when you get tired of typing console.log
 let l = console.log
-// Initialize Firebase
+
+//adds real time clock
 let datetime = null,
         date = null;
 
@@ -14,6 +16,7 @@ datetime = $("#date-time")
     update();
     setInterval(update, 1000);
 
+  // Initialize Firebase
 var config = {
     apiKey: "AIzaSyD_YOyZU-QxptbHgIMs7-_ncQNAdujhwgs",
     authDomain: "ride-the-train-90321.firebaseapp.com",
@@ -27,6 +30,7 @@ var config = {
 
 let trainDatabase = firebase.database();
 
+//adds a new train, data stored on firebase
 $("#gimme-train").on("click", function() {
 
   let trainName = $("#name-here").val().trim();
@@ -35,6 +39,7 @@ $("#gimme-train").on("click", function() {
   let trainTime = moment($("#time-here").val().trim(), "HH:mm").format("X");
   let trainFrequency = $("#frequency-here").val().trim();
 
+   // Creates a local object for holding data temporarily
   let trainAdded = {
     name: trainName,
     destination: trainPlace,
@@ -42,8 +47,10 @@ $("#gimme-train").on("click", function() {
     frequency: trainFrequency,
   }
 
+  //passes data to the firebase realtime database
   trainDatabase.ref().push(trainAdded);
 
+  //clears submitted data after button click
   $("#name-here").val("");
   $("#place-here").val("");
   $("#time-here").val("");
@@ -53,8 +60,10 @@ $("#gimme-train").on("click", function() {
   return false;
 })
 
+//function which invokes when child data is added to the parent(firebase main) data
 trainDatabase.ref().on("child_added", function(childSnapshot){
 
+//stores the snapshot (ie the data at that particular moment) into variables
 let trainName = childSnapshot.val().name;
 let trainPlace = childSnapshot.val().destination;
 let trainTime = childSnapshot.val().time;
@@ -63,18 +72,21 @@ let trainFrequency = childSnapshot.val().frequency;
 let currentTime = moment();
 l(moment(currentTime).format("hh:mm"));
 
+//calculates next arrival and minutes away
 let timeCrunch = moment(trainTime, "hh:mm").subtract(1, "years");
 let minTime = moment().diff(moment(timeCrunch), "minutes");
 let timeApart = minTime % trainFrequency;
 let countDown = trainFrequency - timeApart;
 let arriving = moment().add(countDown, "minutes");
 
+//adds the submitted(and calculated) data to a dynamic table
 $("#time-table").append("<tr><td>" + trainName + 
   "</td><td>" + trainPlace + 
   "</td><td>" + trainFrequency + 
   "</td><td>" + moment(arriving).format("hh:mm A") +
   "</td><td>" + countDown + "</td></tr>");
 
+//in case of errors
 }, function(errorObject) {
   l("Errors handled: " + errorObject.code);
 
